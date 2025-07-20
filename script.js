17d7c787efdcc1b22fd2abccf22bbaf033e47df1
sbwjabene <219176633+sbwjabene@users.noreply.github.com>
2025-07-21 01:15:34 +0200

    Freifeld Fragen Struktur inkl. Logik erstellt
    Es wurden eine Freifeld Frage erstellt, die Validierung der Eingabe,
    der Button um zur nächsten Frage bzw. zur letzten Dankesseite zu gelangen
    und die Speicherung der Antworten im local storage implementiert.

---
+++ script.js
@@ -0,0 +1,35 @@
+// Functions
+
+function nächsteFrage() {
+    frageCounter = JSON.parse(localStorage.getItem("localFrageCounter"));
+    frageCounter += 1;
+    localStorage.setItem("localFrageCounter", JSON.stringify(frageCounter)); // Update den Frage Counter im local storage
+
+    const frageDiv = document.getElementById(`frage_div_${frageCounter}`); // Hole den div der nächsten Frage
+    if (frageDiv) { // Checke ob die nächste Frage Div existiert
+        frageDiv.classList.remove("hidden"); // Remove die hidden class um die Frage anzuzeigen
+        // checke ob die nächste Frage die letzte ist, um die Button-Beschreibung zu ändern
+        nächste_frageDiv = document.getElementById(`frage_div_${frageCounter+1}`);
+        if (nächste_frageDiv) {
+            return;
+        } else {
+            document.getElementById(`button_${frageCounter}`).innerText = "Absenden"; // Ändere den Button Text zu "Absenden" für die letzte Frage
+        }
+    }
+    else {
+        window.location.href = "danke.html"; // Wenn keine weiteren Fragen vorhanden sind, weiterleiten zur Danke-Seite
+    }
+}
+
+
+function submitFreifeldAntwort(minLength, maxLength, frageId, inputAnswer, frageDivId) { //min/max Length zum Validieren, frageId/inputAnswer für local storage, frageDivId für hide/show
+    frageCounter = JSON.parse(localStorage.getItem("localFrageCounter"));
+    if (inputAnswer.length >= minLength && inputAnswer.length <= maxLength) { // Checke ob die Antwort valide ist
+        localStorage.setItem("antwort_" + frageCounter, JSON.stringify(document.getElementById(frageId).innerText)); // Speicher die Frage in local storage
+        localStorage.setItem(frageId, JSON.stringify(inputAnswer)); // Speicher die Antwort in local storage
+        document.getElementById(frageDivId).classList.add("hidden"); // Füge die vorherige Frage zur hidden class hinzu
+        nächsteFrage()
+    } else {
+        alert(`Deine Antwort muss zwischen ${minLength} und ${maxLength} Zeichen liegen.`); // Show error Nachricht
+    }
+}
